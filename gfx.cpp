897e891ae5f72f01d26ff558a776d0cb0d650946
Sebastián Hidalgo <sebahidalgo@live.com.ar>
2024-03-02 08:54:32 -0300

    wip graphics

--- gfx.cpp
+++ gfx.cpp
@@ -206,7 +206,7 @@ void GFX::ShapeWrapper::setShapeInfo(const sf::Color shapeColor, const uIntPair 
 	if (this->shapeType == GFX::ShapeWrapper::ShapeType::CONVEX_SHAPE)
 	{
 		sf::ConvexShape* convexShape = dynamic_cast<sf::ConvexShape*>(shapePtr);
-		//convexShape->setFillColor(sf::Color::Transparent);
+		convexShape->setFillColor(sf::Color::Transparent);
 		convexShape->setOutlineColor(shapeColor);
 		convexShape->setOutlineThickness(2.0);
 
@@ -214,7 +214,7 @@ void GFX::ShapeWrapper::setShapeInfo(const sf::Color shapeColor, const uIntPair 
 		convexShape->setPointCount(pointsList.size());
 		for (const floatPair& pos : pointsList)
 		{
-			convexShape->setPoint(idx, { pos.first,pos.second });
+			convexShape->setPoint(idx, { pos.first ,pos.second });
 			idx++;
 		}
 	}
@@ -349,16 +349,16 @@ void GFX::EntitiesRepMapping::setEntityRepresentationInfo()
 		EntityRepresentation& solarPanelRepresentation = EntityRepresentationByEntityType[solarPanel.getEntityType()];
 
 		ShapeWrapper* shape1 = new ShapeWrapper(GFX::ShapeWrapper::ShapeType::RECTANGLE_SHAPE);
-		shape1->setShapeInfo(sf::Color::Red, { 0,0 }, { entityTypeSizeConverter(EntityTypeWrapper::EntityType::SOLAR_PANEL) });
+		shape1->setShapeInfo(sf::Color::Blue, { 0,0 }, { entityTypeSizeConverter(EntityTypeWrapper::EntityType::SOLAR_PANEL) });
 
 		float shape2Scale = 0.85;
 		ShapeWrapper* shape2 = new ShapeWrapper(GFX::ShapeWrapper::ShapeType::RECTANGLE_SHAPE);
 		floatPair sizeShape2 = { (entityTypeSizeConverter(EntityTypeWrapper::EntityType::SOLAR_PANEL).first * shape2Scale), (entityTypeSizeConverter(EntityTypeWrapper::EntityType::SOLAR_PANEL).first * shape2Scale) };
 		float posShape2 = (entityTypeSizeConverter(EntityTypeWrapper::EntityType::SOLAR_PANEL).first - (sizeShape2.first)) / 2.0;
-		shape2->setShapeInfo(sf::Color::Red, { posShape2,posShape2 }, { sizeShape2 });
+		shape2->setShapeInfo(sf::Color::Blue, { posShape2,posShape2 }, { sizeShape2 });
 
 		ShapeWrapper* shape3 = new ShapeWrapper(GFX::ShapeWrapper::ShapeType::CONVEX_SHAPE);
-		shape3->setShapeInfo(sf::Color::Red, { posShape2,posShape2 }, { {posShape2,posShape2},{posShape2 + sizeShape2.first,posShape2 + sizeShape2.first},{posShape2 + sizeShape2.first,posShape2} });
+		shape3->setShapeInfo(sf::Color::Blue, { posShape2,posShape2 }, { {0,0},{ sizeShape2.first, sizeShape2.first},{sizeShape2.first,0} });
 
 		solarPanelRepresentation.emplace_back(shape1);
 		solarPanelRepresentation.emplace_back(shape2);
@@ -371,16 +371,10 @@ void GFX::EntitiesRepMapping::setEntityRepresentationInfo()
 		EntityRepresentation& accumulatorRepresentation = EntityRepresentationByEntityType[accumulator.getEntityType()];
 
 		ShapeWrapper* shape1 = new ShapeWrapper(GFX::ShapeWrapper::ShapeType::RECTANGLE_SHAPE);
-		shape1->setShapeInfo(sf::Color::Blue, { 0,0 }, { entityTypeSizeConverter(EntityTypeWrapper::EntityType::ACCUMULATOR) });
-
-		/*float shape2Scale = 0.5;
-		ShapeWrapper* shape2 = new ShapeWrapper(GFX::ShapeWrapper::ShapeType::RECTANGLE_SHAPE);
-		floatPair sizeShape2 = { (entityTypeSizeConverter(EntityTypeWrapper::EntityType::ACCUMULATOR).first * shape2Scale), (entityTypeSizeConverter(EntityTypeWrapper::EntityType::ACCUMULATOR).first * shape2Scale) };
-		float posShape2 = (entityTypeSizeConverter(EntityTypeWrapper::EntityType::ACCUMULATOR).first - (sizeShape2.first)) / 2.0;
-		shape2->setShapeInfo(sf::Color::Blue, { posShape2,posShape2 }, { sizeShape2 });*/
+		shape1->setShapeInfo(sf::Color::Red, { 0,0 }, { entityTypeSizeConverter(EntityTypeWrapper::EntityType::ACCUMULATOR) });
 
 		ShapeWrapper* shape2 = new ShapeWrapper(GFX::ShapeWrapper::ShapeType::CIRCLE_SHAPE);
-		shape2->setShapeInfo(sf::Color::Blue, { 0,0 }, entityTypeSizeConverter(EntityTypeWrapper::EntityType::ACCUMULATOR).first / 2.0);
+		shape2->setShapeInfo(sf::Color::Red, { 0,0 }, entityTypeSizeConverter(EntityTypeWrapper::EntityType::ACCUMULATOR).first / 2.0);
 
 		accumulatorRepresentation.emplace_back(shape1);
 		accumulatorRepresentation.emplace_back(shape2);
